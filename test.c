9ab436ead0df1515d6556eeb8f2eceb1b4b0d8bd
Toheeb Biala <tab7v@mtmail.mtsu.edu>
2019-01-23 16:54:11 -0600

    Update test.c

--- test.c
+++ test.c
@@ -1,3 +1,9 @@
+/*
+ Authors: Toheeb Biala, Khem Narayan Poudel, Gabriel Toban
+ This code reads in the data and sort (based on any desired coordinates) 
+ using a variant of the quicksort  in the C standard library.
+*/
+
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
