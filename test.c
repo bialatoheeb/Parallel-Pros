4a67474b8351f9c2ab614dad8b8203179b430533
Toheeb Biala <tab7v@mtmail.mtsu.edu>
2019-01-23 16:48:40 -0600

    Add files via upload

---
+++ test.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+struct data_struct{
+  long int num;
+  long double x;
+  long double y;
+  long double z;
+};
+
+int compare_x(const void* s1, const void* s2){
+  struct data_struct  *p1 = (struct data_struct *) s1;
+  struct data_struct  *p2 = (struct data_struct *) s2;
+  return p1->x > p2->x;
+}
+
+int compare_y(const void* s1, const void* s2){
+  struct data_struct  *p1 = (struct data_struct *) s1;
+  struct data_struct  *p2 = (struct data_struct *) s2;
+  return p1->y > p2->y;
+}
+
+int compare_z(const void* s1, const void* s2){
+  struct data_struct  *p1 = (struct data_struct *) s1;
+  struct data_struct  *p2 = (struct data_struct *) s2;
+  return p1->z > p2->z;
+}
+
+int main(int argc, char* argv[]){ 
+  
+  int num;
+  FILE *fp;
+  int i;
+  struct data_struct temp;
+  
+  if (argc != 3){
+    printf("Needs two arguments. Exting now\n");
+    exit(0);
+  }
+
+  // Number of elements to sort
+  num = atoi(argv[1]);
+
+  // Array of struct
+  struct data_struct* array  = (struct data_struct *) malloc(num * sizeof(struct data_struct));
+  
+  i = 0;
+  if ((fp = fopen(argv[2], "r")) != NULL){
+    while(!feof(fp) && i < num){
+      fscanf(fp, "%lu  %Lf  %Lf %Lf\n", &temp.num, &temp.x, &temp.y, &temp.z);
+      array[i++] = temp;
+    }
+    fclose(fp);
+  }
+  
+  for (i = 0; i < num ; i++)
+    printf("%Lu\t%0.15Lf\t%0.15Lf\t%0.15Lf\n", array[i].num, array[i].x, array[i].y, array[i].z);
+  
+  
+   //qsort(array, num, sizeof(struct data_struct), compare_x);
+   //qsort(array, num, sizeof(struct data_struct), compare_y);
+   qsort(array, num, sizeof(struct data_struct), compare_z);
+
+   for (i = 0; i < num ; i++)
+     printf("%Lu\t%0.15Lf\t%0.15Lf\t%0.15Lf\n", array[i].num, array[i].x, array[i].y, array[i].z);
+   
+}
