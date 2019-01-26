3d50efc2c65572af9766f3986a5aa23bbceba3f2
Toheeb Biala <tab7v@mtmail.mtsu.edu>
2019-01-26 11:32:32 -0600

    Update main.c

--- main.c
+++ main.c
@@ -66,36 +66,9 @@ int main(int argc, char* argv[]){
   long double* L = (long double *) malloc(((num_ranks)*num_ranks)*sizeof(long double));
   
   getL(num_ranks, my_rank, nodeL, Linfo, L);
-  //MPI_Allgather(nodeL, num_ranks+2,MPI_LONG_DOUBLE, Linfo,num_ranks+2,MPI_LONG_DOUBLE, MPI_COMM_WORLD); 
-  //
-  //if (my_rank == 0){
-  //  for (i=0; i< ((num_ranks+2)*num_ranks); i++)
-  //    printf("L[%u]: %0.14Lf\n", i,Linfo[i]);
-  //  
-  //  
-  //
-  //}
-  //int k = 0;
-  //for (i=0; ((num_ranks+2)*num_ranks); i++){
-  //  if (i%(num_ranks+2) == 0){
-  //    for (j=i;j<i+num_ranks; j++){
-  //	if (my_rank == 0)
-  //	  printf("L[%u]: %0.14Lf\n", i,Linfo[j]);
-  //	L[k] = Linfo[j];
-  //	k++;
-  //	if (k >= num_ranks*num_ranks)
-  //	  break;
-  //    }
-  //  }
-  //  if (k >= num_ranks*num_ranks)
-  //    break;
-  //}
-  //printf("\n===========\n\n");
-  //if (my_rank == 0){
-  //  for (i=0; i< (num_ranks*num_ranks); i++)
-  //    printf("L[%u]: %0.14Lf\n", i,L[i]);
-  //}
-  //MPI_Type_free(&array_type);
+  
+  
+  MPI_Type_free(&array_type);
   free(array);
   MPI_Finalize();
 }
