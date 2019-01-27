e5860def0ec0a0e001d72feff67c350977dbf8e7
gtoban <gtoban87@gmail.com>
2019-01-27 16:51:34 -0600

    Cleared Comments

--- main.c
+++ main.c
@@ -39,33 +39,9 @@ int main(int argc, char* argv[]){
   }
   long int* allCounts = (long int *) malloc(num_ranks*num_ranks*sizeof(long int));
   getallCount(num, colIndex, array, allCounts); 
-  // vars for lower bound
-  //long double* nodeL = (long double *) malloc((num_ranks+2)*sizeof(long double));
-  //
-  //getNodeL(num, colIndex, nodeL, array);
-  //printNodeL(nodeL);
-  //
-  ////ALL TO ALL TO GET L
-  //long double* Linfo = (long double *) malloc(((num_ranks+2)*num_ranks)*sizeof(long double));
-  //long double* L = (long double *) malloc(((num_ranks)*num_ranks)*sizeof(long double));
-  //long int* nodeCount = (long int *) malloc(num_ranks*sizeof(long int));
-  //long int* totalCount = (long int *) malloc(num_ranks*sizeof(long int));
-  //long int* allCounts = (long int *) malloc(num_ranks*num_ranks*sizeof(long int));
-  //int balanced;
-  //
-  //getL(nodeL, Linfo, L);
-  //getCounts(num, colIndex, array, L, nodeCount, totalCount, allCounts, &balanced);
-  //
-  //while(balanced == 0){
-  //  adjustLimits(L,totalCount);
-  //  getCounts(num, colIndex, array, L, nodeCount, totalCount, allCounts, &balanced);
-  //}
-  //if (balanced == 1)
-  //  AllToAllSend(array, recv_array, allCounts);
-  //else
-  //  printf("ADJUSTMENT");
-
-  MPI_Type_free(&array_type);
+  AllToAllSend(array, recv_array, allCounts);
+  
+  //MPI_Type_free(&array_type);
   free(array);
   MPI_Finalize();
 }
