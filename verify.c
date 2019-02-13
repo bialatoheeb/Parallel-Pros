3cfbe6bacf0188a1846862b9363c088dfba7f6f2
bialatoheeb <tab7v@mtmail.mtsu.edu>
2019-02-12 20:40:35 -0600

    Update verify

--- verify.c
+++ verify.c
@@ -7,20 +7,20 @@ void verify(long double min, long double max){
   MPI_Allgather(min_max, 2, MPI_LONG_DOUBLE, temp, 2, MPI_LONG_DOUBLE, MPI_COMM_WORLD);
  
   bool = 1;
-  if (my_rank == 0){
-    for (i = 1; i < num_ranks-1; i+=2){
-      if (temp[i] >= temp[i+1]){
-	bool = 0;
-	break;
-      }
+  
+  for (i = 1; i < num_ranks-1; i+=2){
+    if (temp[i] >= temp[i+1]){
+      bool = 0;
+      break;
     }
   }
+
   if (bool == 0){
     if (my_rank == 0)
       printf("Last element on node %d is >= first element on node %d\n", i/2, i/2 + 1);
     MPI_Finalize();
     exit(0);
-	
+    
   }
  
 }
