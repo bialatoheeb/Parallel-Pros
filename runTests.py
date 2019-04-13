d5822bb65c7aaa1f3c2d229145d371d9157ed768
gtoban <gtoban87@gmail.com>
2019-04-13 11:29:21 -0500

    Add files via upload

--- runTests.py
+++ runTests.py
@@ -7,35 +7,54 @@ import time
 #import commands.*
 
 def main():
-    with open("output.txt", "w") as ot:
-        ot.write("DataPoints;Nodes;readTime;1qsort;SortBetweeNodes;TimedataExchange;2qsort;TimeTotal\n")
-    numtest = 20
-    first = 2
+    with open("time.txt", "w") as ot:
+        ot.write("DataPoints;Nodes;createComms;readPoints;getLocalHead;buildGlobalTree;readTargets;getSendSize;buildLocalTree;sendSendSize;assignTargets;localCount;globalCount;totalTime\n")
+    open("error.txt", "w").close()
+    numtest = 1
+    targets = 20000000
+    cpu = "intel.q" #"amd8.q"  qconf -sql
+    first = 32
     ranks = []
-    for i in range(7):
+    rankIncrements = 2
+    for i in range(rankIncrements+1):
         ranks.append(first)
         first = first*2
         #ranks = [i for i in range(8] #,16]
-    datapoints = [i for i in range(20000000,100000001,20000000)] #[40] #, 80, 160]
     
-    for datapoint in datapoints:
-        for rank in ranks:
+    ranks = [16, 32, 64, 128]
+    dpranks = [[10000000,50000000,100000000],[10000000,50000000,100000000],[10000000,50000000,100000000],[50000000,100000000]]
+    ranks = [128]
+    dpranks = [[50000000]]
+    multi = -1
+    #mult = [1,2,5]
+    for rank in ranks:
+        multi += 1
+        #mult = min(int(rank/2),10)
+        #datapoints = [i for i in range(10000000,mult[multi]*20000000 + 1,20000000)] #[40] #, 80, 160]
+        datapoints = dpranks[multi]
+        for datapoint in datapoints:
+            with open("error.txt", "a") as ot:
+                ot.write("\n\n============================\nNEWRUN ")
+                ot.write(str(rank) + " " + str(datapoint) + "\n==============================\n\n")
+        
             testArray = [0.0 for i in range(6)] 
-            my_file = open("test.txt", "w").write("")
+            #my_file = open("test.txt", "a").write("")
             for k in range(numtest):
-                print("datapointe:  ", datapoint, "num_ranks:  ", rank )
+                print("datapoint:  ", datapoint, "num_ranks:  ", rank )
+                with open("time.txt", "a") as ot:
+                    ot.write(str(datapoint)+ ";" + str(rank) + ";")
                 with open("qtest3.sub", "w") as qt:
                     qt.write("!#/bin/bash\n"
 
 
                              "#$ -N main\n"
                              "#$ -pe mpi " + str(rank) + "\n"
-                             "#$ -q amd8.q\n"
+                             "#$ -q "+cpu+"\n"
                              
                              
-                             "mpirun -np $NSLOTS $HOME/COMS7900/clone/main " + str(datapoint) + " >> $HOME/COMS7900/clone/test.txt\n")
+                             "mpirun -np $NSLOTS $HOME/Final/prof_main " + str(datapoint) + " " + str(targets) + " 2>> $HOME/Final/error.txt 1>> $HOME/Final/time.txt  \n")
    
-
+                start = time.time()
                 o = subprocess.call(["qsub", "qtest3.sub"]);
                 p = subprocess.Popen(["qstat"], stdout=subprocess.PIPE);
                 stdout_data = p.communicate()[0]
@@ -43,22 +62,24 @@ def main():
                     #print(stdout_data)
                     p = subprocess.Popen(["qstat"], stdout=subprocess.PIPE);
                     stdout_data = p.communicate()[0]
+                    end = time.time()
+                    print(rank,datapoint,end-start)
                     time.sleep(3)
                     #print(" ")
     
 
-            with open("test.txt", "r") as file2:
-                #file2.readline()
-                allLines = file2.readlines() #.split(",")
-            for line in allLines:
-                temp = line.split(";")
-                for j in range(2,8):
-                  testArray[j-2] += float(temp[j])
-            with open("output.txt", "a") as file2:
-                file2.write(str(datapoint) + ";" + str(rank))
-                for el in testArray:
-                  file2.write(";" + str(el/numtest))
-                file2.write("\n")
+            #with open("test.txt", "r") as file2:
+            #    #file2.readline()
+            #    allLines = file2.readlines() #.split(",")
+            #for line in allLines:
+            #    temp = line.split(";")
+            #    for j in range(2,8):
+            #      testArray[j-2] += float(temp[j])
+            #with open("output.txt", "a") as file2:
+            #    file2.write(str(datapoint) + ";" + str(rank))
+            #    for el in testArray:
+            #      file2.write(";" + str(el/numtest))
+            #    file2.write("\n")
                 
                   
                         
